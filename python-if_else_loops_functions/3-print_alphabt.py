d0caa567e571c5fe24c847a666916db458cbd5e5
Judith <j.espinalm12@gmail.com>
2025-01-19 14:39:47 -0400

    fixing

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if i != 113 and i != 101:  # Exclude ASCII values for 'q' (113) and 'e' (101)
+    if i != 113 and i != 101:
         print("{}".format(chr(i)), end="")
