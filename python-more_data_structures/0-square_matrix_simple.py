ed68dbe4fc672297406da710c1d3b443f2660c7c
Judith <j.espinalm12@gmail.com>
2025-01-26 13:42:56 -0400

    Fixing checkers

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 # Creates a new matrix where each value is the square of the original matrix
-def square_matrix_simple(matrix=[]): # This defines the prototype
+def square_matrix_simple(matrix=[]):  # This defines the prototype
     return [[value ** 2 for value in row] for row in matrix]
