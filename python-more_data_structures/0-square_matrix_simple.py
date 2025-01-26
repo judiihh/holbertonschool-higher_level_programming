f9bbaa335b09ee2dc1d78c8597ffdb020785eb91
Judith Espinal <j.espinalm12@gmail.com>
2025-01-26 13:16:15 -0400

    "Added function that computes the square value of all int of a matrix"

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,3 @@
+# Creates a new matrix where each value is the square of the original matrix
+def square_matrix_simple(matrix=[]): # This defines the prototype
+    return [[value ** 2 for value in row] for row in matrix]
