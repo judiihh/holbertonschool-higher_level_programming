60c2e3a3941442124bdaa3a3b691d51b532fc93d
Judith Espinal <j.espinalm12@gmail.com>
2025-01-26 17:10:37 -0400

    "Separated errors"

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))  # Attempt to format and print as an integer
+        print("{:d}".format(value))
+        # Attempt to format and print as an integer
         return True
-    except (ValueError, TypeError):
+    except ValueError:
+        return False
+    except TypeError:
         return False
