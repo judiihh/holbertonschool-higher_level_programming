4198ef3aaf6e9aff81ae648569a5d38b2a8ff5af
judiihh <j.espinalm12@gmail.com>
2025-02-06 17:29:40 -0800

    Added a class based on task 5

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module containing the BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """A class representing BaseGeometry."""
+
+    def area(self):
+        """Raises an exception indicating the method is not implemented."""
+        raise Exception("area() is not implemented")
