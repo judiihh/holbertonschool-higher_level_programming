bf2c5d70d0798807b5c015e2ac89af9bec0b9ef6
judiihh <j.espinalm12@gmail.com>
2025-02-06 17:26:42 -0800

    Added only sub class of

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module containing the inherits_from function.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if obj is an instance of a subclass of a_class
+    (but not a_class itself), otherwise False.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
