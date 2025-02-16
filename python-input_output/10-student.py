e351bb03f9000e6023b9fb19e739a4de4c844cbe
judiihh <j.espinalm12@gmail.com>
2025-02-16 05:32:23 -0800

    Added class Student defines based on ask 9

---
+++ python-input_output/10-student.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+This module defines the `Student` class, which represents a student
+with first name, last name, and age.
+"""
+
+
+class Student:
+    """
+    Represents a student with first name, last name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance.
+
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        If `attrs` is a list of strings, only those attributes are included.
+        Otherwise, all attributes are returned.
+
+        Args:
+            attrs (list, optional): List of attribute names to retrieve.
+
+        Returns:
+            dict: A dictionary containing the specified attributes.
+        """
+        if isinstance(attrs, list) and all(
+            isinstance(attr, str) for attr in attrs
+        ):
+            return {
+                key: getattr(self, key)
+                for key in attrs if hasattr(self, key)
+            }
+        return self.__dict__
