1e855a46417b38c14c6685eff097df9a5cf19256
judiihh <j.espinalm12@gmail.com>
2025-02-16 05:05:26 -0800

    Fixing task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,8 +6,8 @@ and saves them to the file `add_item.json`.
 
 import sys
 import os
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
 
