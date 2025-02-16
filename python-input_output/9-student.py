f01fe3f2bc82318632066a16385f8b22840d2cca
judiihh <j.espinalm12@gmail.com>
2025-02-16 05:24:35 -0800

    Added a class Student that defines

---
+++ python-input_output/9-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This module defines the `Student` class, which represents a student
+with first name, last name, and age.
+"""
+
+
+class Student:
+    """
+    Represents a student with first name, last name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance.
+
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict: A dict containing all attributes of the Student instance.
+        """
+        return self.__dict__
