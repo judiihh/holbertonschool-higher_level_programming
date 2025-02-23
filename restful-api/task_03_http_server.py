ae426b9f059efa832e23489d037e6e32d435a1fc
Judith <j.espinalm12@gmail.com>
2025-02-23 18:34:16 -0400

    Develop a simple API using Python

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -19,15 +19,19 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         elif self.path == "/status":
             self.send_response(200)
             self.send_header("Content-type", "application/json")
-            self.end_headers()
             status = {"status": "OK"}
-            self.wfile.write(json.dumps(status).encode("utf-8"))
+            response = json.dumps(status).encode("utf-8")
+            self.send_header("Content-Length", str(len(response)))
+            self.end_headers()
+            self.wfile.write(response)
         else:
             self.send_response(404)
             self.send_header("Content-type", "application/json")
-            self.end_headers()
             error_message = {"error": "Endpoint not found"}
-            self.wfile.write(json.dumps(error_message).encode("utf-8"))
+            response = json.dumps(error_message).encode("utf-8")
+            self.send_header("Content-Length", str(len(response)))
+            self.end_headers()
+            self.wfile.write(response)
 
 PORT = 8000
 
