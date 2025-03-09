5b433af42de6942c67276dd1552b569e94857ede
Judith <j.espinalm12@gmail.com>
2025-03-09 18:48:00 -0400

    Fixed script that creates user_0d_1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,4 @@
--- This script creates the MySQL server user user_0d_1 with all privileges.
--- It also creates user_0d_2 with default privileges to satisfy the privileges checker.
+-- This script creates the MySQL server user user_0d_1 with all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
