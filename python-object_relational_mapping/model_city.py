296bda12629d50e797d8860991c94a4a696450f7
judiihh <j.espinalm12@gmail.com>
2025-03-09 18:31:42 -0700

    Corrected 2 blank lines

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -6,6 +6,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from model_state import Base
 
+
 class City(Base):
     """City class linked to the 'cities' table in MySQL database."""
     __tablename__ = 'cities'
