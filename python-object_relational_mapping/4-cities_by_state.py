84e723ca180a502187158afeb49e08fd28ea232a
judiihh <j.espinalm12@gmail.com>
2025-03-09 17:42:53 -0700

    Deleted white blank line

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
         JOIN states ON cities.state_id = states.id
         ORDER BY cities.id ASC;
     """
-    
+
     # Execute the query
     cursor.execute(query)
 
