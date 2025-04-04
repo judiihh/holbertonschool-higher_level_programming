d44c81ebd41a427332f6676615647eea2392fa51
Judith Espinal <j.espinalm12@gmail.com>
2025-04-03 20:08:43 -0400

    refactor: Improve DOM script readability

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -1,1 +1,5 @@
-document.querySelector('header').style.color = '#FF0000'; 
+// Select the header element and store it in a variable
+const header = document.querySelector('header');
+
+// Change the header text color to red
+header.style.color = '#FF0000'; 
